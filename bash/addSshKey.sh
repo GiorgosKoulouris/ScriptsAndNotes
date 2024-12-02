ab96932c1952400285ae284aaeab3dd336995976
Giorgos Koulouris <grgkoulouris@yahoo.gr>
2024-12-02 15:06:32 +0200

    addSshKey now fetches only one host

--- bash/addSshKey.sh
+++ bash/addSshKey.sh
@@ -7,27 +7,23 @@
 # By default, it fetches ecdsa keys. Modify this option to fetch other formats if necessary
 #   Possible values: ecdsa, ed25519, ecdsa-sk, ed25519-sk, or rsa
 #   Multiple types can be fetched if you specify multiple comma-separated algorithms
-# 
+#
 
-privateIPs=("$1")
+ip="$1"
 algorithm=ecdsa
 
-for ip in "${privateIPs[@]}"
-do
-    echo "Fetching $algorithm keys for $ip."
-    newKeys=`(ssh-keyscan -t $algorithm $ip)`
+echo "Fetching $algorithm keys for $ip."
+newKeys=$( (ssh-keyscan -t $algorithm $ip))
+if [ $? -eq 0 ]; then
+    newRsaKey=$( (echo $newKeys | grep ecdsa-sha2))
     if [ $? -eq 0 ]; then
-        newRsaKey=`(echo $newKeys | grep ecdsa-sha2)`
-        if [ $? -eq 0 ]; then
-            grep -vE "^$ip " ~/.ssh/known_hosts > ./tmpfile && mv ./tmpfile ~/.ssh/known_hosts && \
-                echo "Deleted old $algorithm keys for $ip."
-            echo $newRsaKey | tee -a ~/.ssh/known_hosts && \
-                echo "Added new $algorithm keys for $ip."
-        else
-            echo "No keys with $algorithm encryption for $ip."
-        fi
+        grep -vE "^$ip " ~/.ssh/known_hosts >./tmpfile && mv ./tmpfile ~/.ssh/known_hosts &&
+            echo "Deleted old $algorithm keys for $ip."
+        echo $newRsaKey | tee -a ~/.ssh/known_hosts &&
+            echo "Added new $algorithm keys for $ip."
     else
-        echo "Could not connect to $ip."
+        echo "No keys with $algorithm encryption for $ip."
     fi
-    echo
-done
+else
+    echo "Could not connect to $ip."
+fi
